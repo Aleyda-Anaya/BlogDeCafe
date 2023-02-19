ded0cf2957326ffbdf45b48eceb84752ae4c9f69
LorenaAcunaG <lorena.garavito.itg@gmail.com>
2023-02-19 01:04:36 -0500

    [updated] Se actualiza la imagen de cierre de menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btnMenu = document.getElementById("btn-menu");
 const menuNav = document.getElementById("menu-nav");
 const scrollTo = document.getElementsByClassName("scrollTo");
+//./assets/icons/menu-hamburguesa.svg
 
 Array.from(scrollTo).forEach(el => {
   el.addEventListener("click", function (e) {
@@ -16,5 +17,11 @@ Array.from(scrollTo).forEach(el => {
 btnMenu.addEventListener("click", mostrarMenu);
 
 function mostrarMenu (){
-  menuNav.classList.toggle("mostrar-menu")
+  menuNav.classList.toggle("mostrar-menu");
+  const srcImagen =  btnMenu.children[0].getAttribute("src")
+  if(srcImagen === "./assets/icons/menu-hamburguesa.svg"){
+    btnMenu.children[0].setAttribute("src", "./assets/icons/cerrar-menu.svg");
+  }else{
+    btnMenu.children[0].setAttribute("src", "./assets/icons/menu-hamburguesa.svg");
+  }
 }
